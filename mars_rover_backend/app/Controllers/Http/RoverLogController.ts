2cc03bf6c8e060321ac75289aa0b6837f60355ce
Davi Cintra <davicintra@Davis-MacBook-Air.local>
2023-02-14 02:11:20 -0300

    Created basic api structure/created movementLogic

---
+++ mars_rover_backend/app/Controllers/Http/RoverLogController.ts
@@ -0,0 +1,76 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import RoverLog from 'App/Models/RoverLog'
+
+const calculate_current_position = (instruction: string, inputed_position: string) => {
+    const directions = ['N', 'E', 'S', 'W']
+    const [x, y, direction] = inputed_position.split(' ')
+    const instructions = instruction.split('')
+    let directionIndex = directions.indexOf(direction)
+    let currentX = parseInt(x)
+    let currentY = parseInt(y)
+    let current_direction = direction
+    instructions.forEach((single_instruction) => {
+        if (single_instruction === 'L') {
+            if (directionIndex === 0) {
+                current_direction = directions[directions.length - 1]
+            }else {
+                current_direction = directions[directionIndex - 1]
+            }
+        }
+        
+        if (single_instruction === 'R') {
+            if (directionIndex === directions.length - 1) {
+                current_direction = directions[0]
+            }else {
+                current_direction = directions[directionIndex + 1]
+            }
+        }
+
+        if (single_instruction === 'M') {
+            if (current_direction === 'N') {
+                currentY += 1
+            }
+            if (current_direction === 'E') {
+                currentX += 1
+            }
+            if (current_direction === 'S') {
+                currentY -= 1
+            }
+            if (current_direction === 'W') {
+                currentX -= 1
+            }
+        }
+
+        directionIndex = directions.indexOf(current_direction)
+
+    })
+
+    return `${currentX} ${currentY} ${current_direction}`
+}
+        
+export default class RoverLogController {
+    public static async index ({ response }: HttpContextContract) {
+        const roverLogs = await RoverLog.all()
+        return response.status(200).send(roverLogs)
+    }
+
+    public static async store ({ request, response }: HttpContextContract) {
+        const { instruction, inputed_position } = request.all()
+        const current_position = calculate_current_position(instruction, inputed_position)
+        const roverLog = await RoverLog.create({ instruction, inputed_position, current_position })
+        return response.status(200).send(roverLog)
+    }
+
+    public static async show ({ request, response }: HttpContextContract) {
+        const roverLog = await RoverLog.findOrFail(request.param('id'))
+        return response.status(200).send(roverLog)
+    }
+
+
+    public static async destroy ({ request, response }: HttpContextContract) {
+        const roverLog = await RoverLog.findOrFail(request.param('id'))
+        await roverLog.delete()
+        return response.status(200).send(roverLog)
+    }
+    
+}
---
+++ mars_rover_backend/start/routes/roverLog.route.ts
@@ -0,0 +1,10 @@
+import Route from '@ioc:Adonis/Core/Route'
+import RoverLogController from 'App/Controllers/Http/RoverLogController'
+
+Route.get('/roverLog', RoverLogController.index)
+
+Route.get('/roverLog/:id', RoverLogController.show)
+
+Route.post('/roverLog', RoverLogController.store)
+
+Route.delete('/roverLog/:id', RoverLogController.destroy)
